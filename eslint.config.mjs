b7755911396ab2bbf8b9194d1f7e576e92e81a0d
Ashish Kumar Saini <mashish2608@gmail.com>
2025-09-01 00:48:46 +0530

    fix lint erors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,19 +12,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    "rules": {
+  ...compat.config({
+    extends: ['next'],
+    rules: {
       /* TypeScript */
       "@typescript-eslint/explicit-function-return-type": "warn", // forces return types (good for APIs/libraries)
       "@typescript-eslint/no-explicit-any": "warn", // avoid `any`, but allow if needed
       "@typescript-eslint/no-non-null-assertion": "warn", // prevents `!` misuse
-      "@typescript-eslint/consistent-type-imports": [
-        "error",
-        {
-          "prefer": "type-imports",
-          "fixStyle": "inline-type-imports"
-        }
-      ],
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
@@ -65,7 +59,9 @@ const eslintConfig = [
         }
       ],
       "react-hooks/exhaustive-deps": "warn"
-    },
+    }
+  }),
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
